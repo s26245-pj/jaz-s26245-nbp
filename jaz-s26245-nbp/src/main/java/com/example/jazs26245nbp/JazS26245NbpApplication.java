a0b615e96bc1fa60d5938ea0e927d49623066178
s26245-pj <s26245@pjwstk.edu.pl>
2023-06-26 18:39:38 +0200

    created new project

---
+++ jaz-s26245-nbp/src/main/java/com/example/jazs26245nbp/JazS26245NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs26245nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS26245NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS26245NbpApplication.class, args);
+	}
+
+}
